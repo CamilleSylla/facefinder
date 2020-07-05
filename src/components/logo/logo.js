cbf1256c4ef8d09ebecc60106363c26f3aadc9db
CamilleSylla <camillesylla.web@gmail.com>
2020-07-05 12:26:36 +0200

    added the logo and create new file image link form

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tilt from'react-tilt';
+import "./logo.css";
+import logo from './logo.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-        <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-        <div className="Tilt-inner"> 👽 </div>
+        <Tilt className="Tilt br2" options={{ max : 80 }} style={{ height: 250, width: 250 }} >
+        <div className="Tilt-inner pa3">
+            <img style={{paddingTop: '5px', width: '70%'}} alt=" logo" src={logo}/> 
+            </div>
         </Tilt>
         </div>
     )
