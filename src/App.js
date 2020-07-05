22023f5c51428eac63118e43f18f1c57bc9f764a
CamilleSylla <camillesylla.web@gmail.com>
2020-07-05 13:13:32 +0200

    added particules to the project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import './App.css';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles : {
+    line_linked: {
+      shadow :{
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles 
+                params={particlesOptions} />
+        
      <Navigation/>
      <Logo/>
+     <Rank/>
      <ImageLinkForm/>
      {/*<FaceRecognition/>*/}
     </div>
